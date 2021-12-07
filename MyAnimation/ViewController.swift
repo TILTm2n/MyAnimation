3b55d3cf9d074cab6facee8ef3a32a13767d55a4
Eugene <evgen29031999@bk.ru>
2021-12-07 23:24:36 +0300

    rotate animation

--- MyAnimation/ViewController.swift
+++ MyAnimation/ViewController.swift
@@ -9,9 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewForAnimate: UIView!
+    
+    var animator: UIViewPropertyAnimator!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        viewForAnimate.center = view.center
+        viewForAnimate.layer.backgroundColor = UIColor.red.cgColor
+        viewForAnimate.frame.size.height = 0
+        viewForAnimate.frame.size.width = 0
+        
+        animator = UIViewPropertyAnimator(duration: 3, curve: .easeInOut, animations: {
+            self.viewForAnimate.frame.size.height = 128
+            self.viewForAnimate.frame.size.width = 240
+            self.viewForAnimate.layer.backgroundColor = UIColor.blue.cgColor
+            self.viewForAnimate.transform = CGAffineTransform(rotationAngle: .pi)
+            self.viewForAnimate.center = self.view.center
+        })
+        
+        animator.startAnimation()
+        
     }
 
 
